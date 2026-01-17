f6054abb613e6e7da926df72eb91fe8e48249ffe
mingli202 <vincentmingli@gmail.com>
2026-01-17 14:57:31 -0500

    fixx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Auth0Provider } from "@auth0/nextjs-auth0";
+import ConvexAndAuth0Integration from "@/integrations/convex";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Auth0Provider>{children}</Auth0Provider>
+        <ConvexAndAuth0Integration>{children}</ConvexAndAuth0Integration>
       </body>
     </html>
   );
